2c26a1ee8b6b3a2f94a5b9481ab2c9c2f385219b
Madxmike <mep053@gmail.com>
2022-01-31 17:50:03 -0500

    GET -> POST

--- src/main/java/com/lasik/parser/ParsingController.java
+++ src/main/java/com/lasik/parser/ParsingController.java
@@ -7,6 +7,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
@@ -16,7 +17,7 @@ import javax.json.Json;
 @Controller
 public final class ParsingController {
 
-    @GetMapping(value = "/parse", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/parse", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public ResponseEntity<StreamingResponseBody> parse(@RequestBody String code) throws ParseProblemException {
         final var parsed = StaticJavaParser.parse(code);
