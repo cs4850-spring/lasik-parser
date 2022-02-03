fff7a353c679b83b6f7841209ccb90cbc8e96582
Madxmike <mep053@gmail.com>
2022-02-03 06:20:12 -0500

    Add security config

--- src/main/java/com/lasik/parser/LasikParserApplication.java
+++ src/main/java/com/lasik/parser/LasikParserApplication.java
@@ -2,9 +2,6 @@ package com.lasik.parser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 public class LasikParserApplication {
