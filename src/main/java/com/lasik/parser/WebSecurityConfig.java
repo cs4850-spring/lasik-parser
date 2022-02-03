571b00f7a3b0e992dcddcb9184c50564f3ba69bd
Madxmike <mep053@gmail.com>
2022-02-03 05:36:58 -0500

    Add security config

--- src/main/java/com/lasik/parser/WebSecurityConfig.java
+++ src/main/java/com/lasik/parser/WebSecurityConfig.java
@@ -1,18 +1,14 @@
 package com.lasik.parser;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@SpringBootApplication
-public class LasikParserApplication {
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    public static void main(String[] args) {
-        SpringApplication.run(LasikParserApplication.class, args);
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors();
     }
-
-
-
 }
