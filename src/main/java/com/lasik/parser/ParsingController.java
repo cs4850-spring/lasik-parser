cb1af9031e6885e9311d8c3f81f43c3e10293365
Madxmike <mep053@gmail.com>
2022-02-03 04:16:33 -0500

    Remove origin

--- src/main/java/com/lasik/parser/ParsingController.java
+++ src/main/java/com/lasik/parser/ParsingController.java
@@ -5,17 +5,16 @@ import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.serialization.JavaParserJsonSerializer;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import javax.json.Json;
 
-@Controller
+@RestController
 public final class ParsingController {
 
     @PostMapping(value = "/parse", produces = MediaType.APPLICATION_JSON_VALUE)
-    @CrossOrigin(origins = "http://lasik.michaelepps.me/", methods = RequestMethod.POST)
+    @CrossOrigin
     public @ResponseBody ResponseEntity<StreamingResponseBody> parse(@RequestBody String code) throws ParseProblemException {
         final var parsed = StaticJavaParser.parse(code);
 
